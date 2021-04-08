98b1a880ef213abb49af9f9bd5fcff6d13729c21
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-04-07 21:15:14 -0500

    chore(deps): release-config-npm@1.0.1
    replace inline config with codedependant npm config for semantic release

--- release.config.js
+++ release.config.js
@@ -2,43 +2,8 @@
 
 /* istanbul ignore file */
 module.exports = {
-  branches: [
-    'master'
-  ]
-, plugins: [
-    ['@semantic-release/commit-analyzer', {
-      parserOpts: {
-        noteKeywords: ['BREAKING', 'BREAKING CHANGE', 'BREAKING CHANGES']
-      , referenceActions: [
-          'close', 'closes', 'closed'
-        , 'fix', 'fixes', 'fixed'
-        , 'resolve', 'resolves', 'resolved'
-        ]
-      }
-    , releaseRules: [
-        {type: 'build', release: 'patch'}
-      , {type: 'ci', release: 'patch'}
-      , {type: 'release', release: 'patch'}
-      , {type: 'chore', release: 'patch'}
-      , {type: 'refactor', release: 'patch'}
-      , {type: 'test', release: 'patch'}
-      , {type: 'doc', release: 'patch'}
-      , {type: 'lib', release: 'patch'}
-      ]
-    }],
-  , ['@semantic-release/release-notes-generator', null]
-  , ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md'
-    , changelogTitle: '# Semantic Release Docker'
-    }]
-  , ['@semantic-release/npm', null]
-  , ['@semantic-release/git', {
-      assets: [
-        'package.json'
-      , 'pnpm-lock.yaml'
-      , 'CHANGELOG.md'
-      ]
-    }]
-  , ['@semantic-release/github', null]
-  ]
+  branches: ['master']
+, extends: '@codedependant/release-config-npm'
+, changelogFile: 'CHANGELOG.md'
+, changelogTitle: '# Semantic Release Docker'
 }
