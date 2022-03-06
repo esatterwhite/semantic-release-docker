4646bd681cb4d7ee4a48a9187fbe7dfe88686b78
Eric Satterwhite <esatterwhite@wi.rr.com>
2022-03-06 15:43:15 -0600

    feat(docker): allow command to be run via docker in verify step
    Expose a setting for a command to be run in the build image during the
    verify stage. During a dry run, the prepare stage is never run, which is
    the stage that actualy builds the image.
    
    fixes: #20

--- test/integration/verify.js
+++ test/integration/verify.js
@@ -8,21 +8,29 @@ const buildConfig = require('../../lib/build-config.js')
 const verify = require('../../lib/verify.js')
 const DOCKER_REGISTRY_HOST = process.env.TEST_DOCKER_REGISTRY || 'localhost:5000'
 
+const logger = {
+  success: sinon.stub()
+, debug: sinon.stub()
+, info: sinon.stub()
+, fatal: sinon.stub()
+}
+
 test('steps::verify', async (t) => {
   const build_id = crypto.randomBytes(5).toString('hex')
   t.test('docker password, no username', async (tt) => {
     const context = {
       env: {
         ...process.env
+      , GITHUB_TOKEN: ''
       , DOCKER_REGISTRY_PASSWORD: 'abc123'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
-    const config = await buildConfig(build_id, {}, context)
+    const config = await buildConfig(build_id, {
+      dockerRegistry: DOCKER_REGISTRY_HOST
+    }, context)
+
     await tt.rejects(
       verify(config, context)
     , {
@@ -33,20 +41,22 @@ test('steps::verify', async (t) => {
     )
   })
 
-  t.test('docker password, no username', async (tt) => {
+  t.test('docker username, no password', async (tt) => {
     const context = {
       env: {
         ...process.env
+      , GITHUB_TOKEN: ''
       , DOCKER_REGISTRY_USER: 'abc123'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
-    const config = await buildConfig(build_id, {}, context)
-    await tt.rejects(
+
+    const config = await buildConfig(build_id, {
+      dockerRegistry: DOCKER_REGISTRY_HOST
+    }, context)
+
+    tt.rejects(
       verify(config, context)
     , {
         message: /docker authentication failed/i
@@ -64,14 +74,12 @@ test('steps::verify', async (t) => {
       , DOCKER_REGISTRY_PASSWORD: 'secretsquirrel'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
     const config = await buildConfig(build_id, {
       dockerRegistry: DOCKER_REGISTRY_HOST
     }, context)
+
     tt.resolves(verify(config, context))
   })
 
@@ -83,10 +91,7 @@ test('steps::verify', async (t) => {
       , GITHUB_TOKEN: 'secretsquirrel'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
     const config = await buildConfig(build_id, {
       dockerRegistry: DOCKER_REGISTRY_HOST
@@ -101,10 +106,7 @@ test('steps::verify', async (t) => {
       , DOCKER_REGISTRY_USER: 'iamweasel'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
     const config = await buildConfig(build_id, {
       dockerRegistry: DOCKER_REGISTRY_HOST
@@ -121,10 +123,7 @@ test('steps::verify', async (t) => {
       , DOCKER_REGISTRY_USER: 'abc123'
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
     const config = await buildConfig(build_id, {}, context)
     tt.rejects(
@@ -141,16 +140,14 @@ test('steps::verify', async (t) => {
     const context = {
       env: {
         ...process.env
+      , GITHUB_TOKEN: ''
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
 
     const config = await buildConfig(build_id, {}, context)
-    tt.strictEqual(await verify(config, context), false, 'auth step skipped')
+    tt.strictEqual(await verify(config, context), true, 'auth step skipped')
   })
 
   t.test('unable to collect image name', async (tt) => {
@@ -159,10 +156,7 @@ test('steps::verify', async (t) => {
         ...process.env
       }
     , cwd: __dirname
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
 
     const config = await buildConfig(build_id, {
@@ -185,10 +179,7 @@ test('steps::verify', async (t) => {
         ...process.env
       }
     , cwd: process.cwd()
-    , logger: {
-        success: sinon.stub()
-      , info: sinon.stub()
-      }
+    , logger: logger
     }
 
     const config = await buildConfig(build_id, {
@@ -201,6 +192,6 @@ test('steps::verify', async (t) => {
     , name: 'SemanticReleaseError'
     , details: /relative to pwd/gi
     })
-
   })
+
 }).catch(threw)
